5948479d21f85ccc70f77b4a461852147d8be573
Alexander Wrede <alexander.wrede@venatus247.de>
2025-03-16 15:02:36 +0100

    Add function documentation

--- c/matrix/matrix.h
+++ c/matrix/matrix.h
@@ -6,24 +6,122 @@ typedef struct {
   double *data;
 } marray;
 
+/*
+Frees the memory of the given matrix
+
+Input:
+  marray* m: a pointer to a matrix
+
+Output:
+  None
+*/
 void matrix_free(marray* m);
 
+/*
+Prints the given matrix to console
+
+Input:
+  marray* m: a pointer to a matrix
+
+Output:
+  None, but the matrix is displayed in the console
+*/
 void matrix_print(const marray* m);
 
+/*
+Creates a new matrix with given dimension (rows x cols) with value zero in each element
+
+Input:
+  int rows: amount of rows the matrix will have
+  int cols: amount of columns the matrix will have
+
+Output:
+  marray*: a pointer to the created matrix with given dimensions and value zero in each element
+*/
 marray* matrix_zeroes(int rows, int cols);
 
+/*
+Adds the value b to the matrix a
+
+Input:
+  marray* a: matrix to which the matrix b is added
+  double b: value that is added to the matrix a
+
+Output:
+  None, but the matrix a is the result of the addition
+*/
 void matrix_addi_val(const marray* a, double b);
 
+/*
+Calculates the result of the matrix a, to which value b is added
+
+Input:
+  marray* a: matrix to which the value b is added
+  double b: value that is added to the matrix a
+
+Output:
+  marray*: the result of the addition of matrix a and value b
+*/
 marray* matrix_add_val(const marray* a, double b);
 
+/*
+Adds the second b matrix to the first matrix a
+
+Input:
+  marray* a: matrix to which the matrix b is added
+  marray* b: matrix that is added to the matrix a
+
+Output:
+  None, but the matrix a is the result of the matrix addition of a and b
+*/
 void matrix_addi(const marray* a, const marray* b);
 
+/*
+Calculates the matrix addition of a and b
+
+Input:
+  marray* a: matrix to which the matrix b is added
+  marray* b: matrix that is added to the matrix a
+
+Output:
+  marray*: the result of the matrix addition of a and b
+*/
 marray* matrix_add(const marray* a, const marray* b);
 
+/*
+Subtracts the second b matrix to the first matrix a
+
+Input:
+  marray* a: matrix to which the matrix b is subtracted
+  marray* b: matrix that is subtracted from the matrix a
+
+Output:
+  None, but the matrix a is the result of the matrix subtraction of a and b
+*/
 void matrix_subi(const marray* a, const marray* b);
 
+/*
+Calculates the matrix subtraction of a and b
+
+Input:
+  marray* a: matrix to which the matrix b is subtracted
+  marray* b: matrix that is subtracted from the matrix a
+
+Output:
+  marray*: the result of the matrix addition of a and b
+*/
 marray* matrix_sub(const marray* a, const marray* b);
 
-marray* matrix_dot(const marray *a, const marray *b);
+/*
+Calculates the dot product of two matrices
+
+Input:
+  marray* a: a pointer to a matrix
+  marray* b: a pointer to a matrix
+
+Output:
+  marray*: the result of the matrix dot product a*b
+*/
+marray* matrix_dot(const marray* a, const marray* b);
 
 #endif //MATRIX_H
