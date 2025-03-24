7951c665713b54572f0a110bbcef05d609c57d1b
Alexander Wrede <alexander.wrede@venatus247.de>
2025-03-24 11:26:08 +0100

    Add basic complex matrix functions
    Functions to create, free and print a complex matrix

---
+++ c/complex/complex.h
@@ -0,0 +1,57 @@
+#ifndef COMPLEX_H
+#define COMPLEX_H
+
+#include <complex.h>
+
+typedef struct {
+    int rows, cols;
+    double complex *data;
+} carray;
+
+/*
+Frees the memory of the given matrix
+
+Input:
+    carray *m: pointer to a matrix
+
+Output:
+    None
+*/
+void cmatrix_free(carray *m);
+
+/*
+Prints the given matrix to console
+
+Input:
+    carray *m: pointer to a matrix
+
+Output:
+    None, but the matrix is displayed in the console
+*/
+void cmatrix_print(const carray *m);
+
+/*
+Prints the given matrix to console with given precision
+
+Input:
+    carray *m: pointer to a matrix
+    int precision: amount of decimals should be printed
+
+Output:
+    None, but the matrix is displayed in the console with given decimal precision
+*/
+void cmatrix_print_precision(const carray *m, int precision);
+
+/*
+Creates a new complex matrix with given dimension (rows x cols) with value zero in each element
+
+Input:
+    int rows: amount of rows the matrix will have
+    int cols: amount of columns the matrix will have
+
+Output:
+    marray*: pointer to the created complex matrix with given dimensions and value zero in each element
+*/
+carray *cmatrix_zeroes(int rows, int cols);
+
+#endif //COMPLEX_H
