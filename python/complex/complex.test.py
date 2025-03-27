f6842abecadc7fe490982fec9406a7fde4648276
Alexander Wrede <alexander.wrede@venatus247.de>
2025-03-27 13:17:49 +0100

    Add fourier transform in python
    Calculates the Fourier Transform with the Fast Fourier Transform or the Discret Fourier Transform

--- python/complex/complex.test.py
+++ python/complex/complex.test.py
@@ -6,15 +6,15 @@ import complex as c
 
 
 class MyTestCase(unittest.TestCase):
-    f_4 = c.dft_matrix(4)
-    f_5 = c.dft_matrix(5)
-    f_10 = c.dft_matrix(10)
+    f_4 = c.fft.dft_matrix(4, norm='ortho')
+    f_5 = c.fft.dft_matrix(5, norm='ortho')
+    f_10 = c.fft.dft_matrix(10, norm='ortho')
 
     eye_4 = np.eye(4)
 
     rnd_matrix = np.random.randn(4, 3)
 
-    def test_dft(self):
+    def test_dft_matrix(self):
         # TODO add test cases
         self.assertEqual(True, True)  # add assertion here
 
@@ -25,6 +25,16 @@ class MyTestCase(unittest.TestCase):
 
         self.assertTrue(c.is_unitary(self.eye_4))
         self.assertFalse(c.is_unitary(self.rnd_matrix))
+
+    def test_fft(self):
+        random = np.random.randn(64)
+        self.assertTrue(np.allclose(c.fft.fft(random), np.fft.fft(random)))
+        self.assertTrue(np.allclose(c.fft.fft(random, norm='ortho'), np.fft.fft(random, norm='ortho')))
+
+    def test_dft(self):
+        random = np.random.randn(63)
+        self.assertTrue(np.allclose(c.fft.dft(random), np.fft.fft(random)))
+        self.assertTrue(np.allclose(c.fft.dft(random, norm='ortho'), np.fft.fft(random, norm='ortho')))
 
 if __name__ == '__main__':
     unittest.main()
